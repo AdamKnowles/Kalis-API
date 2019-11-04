759c4b3aaa9e24111a62def86cf0156e01d3b434
Adam Knowles <acknowles524@gmail.com>
2019-11-03 21:29:49 -0600

    added alerts for assessment and patient data

--- kalisAPI/views/assessment.py
+++ kalisAPI/views/assessment.py
@@ -42,37 +42,40 @@ class Assessments(ViewSet):
         Returns:
             Response -- JSON serialized vital sign instance
         """
-        new_assessment = Assessment()
-        new_assessment.time = datetime.now()
-        new_assessment.mental_status = MentalStatus.objects.get(
-            pk=request.data['mental_status'])
-        new_assessment.heart_sounds = HeartSounds.objects.get(
-            pk=request.data['heart_sounds'])
-        new_assessment.pupil_response = PupilResponse.objects.get(
-            pk=request.data['pupil_response']) 
-        new_assessment.breath_sounds = BreathSounds.objects.get(
-            pk=request.data['breath_sounds'])
-        new_assessment.edema = Edema.objects.get(
-            pk=request.data['edema'])
-        new_assessment.oxygen_rate = OxygenRate.objects.get(
-            pk=request.data['oxygen_rate'])
-        new_assessment.bowel_sounds = BowelSounds.objects.get(
-            pk=request.data['bowel_sounds'])
-        new_assessment.npo = Npo.objects.get(
-            pk=request.data['npo'])
-        new_assessment.last_bowel_movement = request.data["last_bowel_movement"]
-        new_assessment.urine_color = UrineColor.objects.get(
-            pk=request.data['urine_color'])
-        new_assessment.urine_odor = UrineOdor.objects.get(
-            pk=request.data['urine_odor'])
-        new_assessment.urine_amount = request.data["urine_amount"]
-        patient = Patient.objects.get(pk=request.data["patient_id"])
-        new_assessment.patient = patient
-        new_assessment.save()
+        if datetime.today() >= datetime.strptime(request.data["last_bowel_movement"], '%Y-%m-%d'):
+            new_assessment = Assessment()
+            new_assessment.time = datetime.now()
+            new_assessment.mental_status = MentalStatus.objects.get(
+                pk=request.data['mental_status'])
+            new_assessment.heart_sounds = HeartSounds.objects.get(
+                pk=request.data['heart_sounds'])
+            new_assessment.pupil_response = PupilResponse.objects.get(
+                pk=request.data['pupil_response']) 
+            new_assessment.breath_sounds = BreathSounds.objects.get(
+                pk=request.data['breath_sounds'])
+            new_assessment.edema = Edema.objects.get(
+                pk=request.data['edema'])
+            new_assessment.oxygen_rate = OxygenRate.objects.get(
+                pk=request.data['oxygen_rate'])
+            new_assessment.bowel_sounds = BowelSounds.objects.get(
+                pk=request.data['bowel_sounds'])
+            new_assessment.npo = Npo.objects.get(
+                pk=request.data['npo'])
+            new_assessment.last_bowel_movement = request.data["last_bowel_movement"]
+            new_assessment.urine_color = UrineColor.objects.get(
+                pk=request.data['urine_color'])
+            new_assessment.urine_odor = UrineOdor.objects.get(
+                pk=request.data['urine_odor'])
+            new_assessment.urine_amount = request.data["urine_amount"]
+            patient = Patient.objects.get(pk=request.data["patient_id"])
+            new_assessment.patient = patient
+            new_assessment.save()
 
-        serializer = AssessmentSerializer(new_assessment, context={'request': request})
+            serializer = AssessmentSerializer(new_assessment, context={'request': request})
 
-        return Response(serializer.data)
+            return Response(serializer.data)
+        else:
+            return Response({"error" : "Can not enter BM date in the future"}, status=status.HTTP_400_BAD_REQUEST)    
 
     def retrieve(self, request, pk=None):
         """Handle GET requests for single vital sign set
@@ -94,34 +97,39 @@ class Assessments(ViewSet):
         Returns:
             Response -- Empty body with 204 status code
         """
-        updated_assessment = Assessment.objects.get(pk=pk)
-        updated_assessment.mental_status = MentalStatus.objects.get(
-            pk=request.data['mental_status'])
-        updated_assessment.heart_sounds = HeartSounds.objects.get(
-            pk=request.data['heart_sounds'])
-        updated_assessment.pupil_response = PupilResponse.objects.get(
-            pk=request.data['pupil_response']) 
-        updated_assessment.breath_sounds = BreathSounds.objects.get(
-            pk=request.data['breath_sounds'])
-        updated_assessment.edema = Edema.objects.get(
-            pk=request.data['edema'])
-        updated_assessment.oxygen_rate = OxygenRate.objects.get(
-            pk=request.data['oxygen_rate'])
-        updated_assessment.bowel_sounds = BowelSounds.objects.get(
-            pk=request.data['bowel_sounds'])
-        updated_assessment.npo = Npo.objects.get(
-            pk=request.data['npo'])
-        updated_assessment.last_bowel_movement = request.data["last_bowel_movement"]
-        updated_assessment.urine_color = UrineColor.objects.get(
-            pk=request.data['urine_color'])
-        updated_assessment.urine_odor = UrineOdor.objects.get(
-            pk=request.data['urine_odor'])
-        updated_assessment.urine_amount = request.data["urine_amount"]
-        patient = Patient.objects.get(pk=request.data["patient_id"])
-        updated_assessment.patient = patient
-        updated_assessment.save()
+        if datetime.today() >= datetime.strptime(request.data["last_bowel_movement"], '%Y-%m-%d'):
+            updated_assessment = Assessment.objects.get(pk=pk)
+            updated_assessment.mental_status = MentalStatus.objects.get(
+                pk=request.data['mental_status'])
+            updated_assessment.heart_sounds = HeartSounds.objects.get(
+                pk=request.data['heart_sounds'])
+            updated_assessment.pupil_response = PupilResponse.objects.get(
+                pk=request.data['pupil_response']) 
+            updated_assessment.breath_sounds = BreathSounds.objects.get(
+                pk=request.data['breath_sounds'])
+            updated_assessment.edema = Edema.objects.get(
+                pk=request.data['edema'])
+            updated_assessment.oxygen_rate = OxygenRate.objects.get(
+                pk=request.data['oxygen_rate'])
+            updated_assessment.bowel_sounds = BowelSounds.objects.get(
+                pk=request.data['bowel_sounds'])
+            updated_assessment.npo = Npo.objects.get(
+                pk=request.data['npo'])
+            updated_assessment.last_bowel_movement = request.data["last_bowel_movement"]
+            updated_assessment.urine_color = UrineColor.objects.get(
+                pk=request.data['urine_color'])
+            updated_assessment.urine_odor = UrineOdor.objects.get(
+                pk=request.data['urine_odor'])
+            updated_assessment.urine_amount = request.data["urine_amount"]
+            patient = Patient.objects.get(pk=request.data["patient_id"])
+            updated_assessment.patient = patient
+            updated_assessment.save()
 
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
+            serializer = AssessmentSerializer(updated_assessment, context={'request': request})
+
+            return Response(serializer.data)
+        else:
+            return Response({"error" : "Can not have birth date in the future"}, status=status.HTTP_400_BAD_REQUEST)    
 
     def destroy(self, request, pk=None):
         """Handle DELETE requests for a patient
--- kalisAPI/views/patient.py
+++ kalisAPI/views/patient.py
@@ -7,6 +7,7 @@ from kalisAPI.models import Patient, VitalSigns
 from .vitalsigns import VitalSignSerializer
 from rest_framework.decorators import action
 from django.db import models
+from datetime import datetime
 
 
 
@@ -42,17 +43,20 @@ class Patients(ViewSet):
         Returns:
             Response -- JSON serialized patient instance
         """
-        new_patient = Patient()
-        new_patient.first_name = request.data["first_name"]
-        new_patient.last_name = request.data["last_name"]
-        new_patient.birth_date = request.data["birth_date"]
-        new_patient.sex = request.data["sex"]
-        new_patient.diagnosis = request.data["diagnosis"].lower()
-        new_patient.save()
+        if datetime.today() >= datetime.strptime(request.data["birth_date"], '%Y-%m-%d'):
+            new_patient = Patient()
+            new_patient.first_name = request.data["first_name"]
+            new_patient.last_name = request.data["last_name"]
+            new_patient.birth_date = request.data["birth_date"]
+            new_patient.sex = request.data["sex"]
+            new_patient.diagnosis = request.data["diagnosis"].lower()
+            new_patient.save()
 
-        serializer = PatientSerializer(new_patient, context={'request': request})
+            serializer = PatientSerializer(new_patient, context={'request': request})
 
-        return Response(serializer.data)
+            return Response(serializer.data)
+        else:
+            return Response({"error" : "Can not have birth date in the future"}, status=status.HTTP_400_BAD_REQUEST)    
 
     def retrieve(self, request, pk=None):
         """Handle GET requests for single patient
